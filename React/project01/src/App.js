da52354f9cacb0cc49af2297540ef6148030b70e
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-03-09 18:52:56 -0300

    load

--- React/project01/src/App.js
+++ React/project01/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+<style>
+
+</style>
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Home />
+
+
+
     </div>
   );
 }
 
 export default App;
+
+const Home = () => {
+  return <div>
+    <header>
+    <section>
+      <h1>Henrique de Araújo Griebeler</h1>
+      <p id='sectionP1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </section>
+    </header>
+
+      <div> {/* AQUI VAI EXPLICANDO COISAS DO CURRICULO */}  
+      
+      <br></br><br></br><br></br>
+      <br></br> <br></br> <br></br> <br></br> <br></br>  </div>
+
+        <div>
+        <main>
+          <div id='boxFrame'>
+            <div class='box'><img src='../images/html.png'></img>HTML 5</div>
+            <div class='box'><img src='../images/css.png'></img>CSS 3</div>
+            <div class='box'><img src='../images/javaScript.png'></img>JavaScript</div>
+            <div class='box'><img src='../images/reactjs.png'></img>React.JS</div>
+            </div>
+        </main>
+        </div>
+
+  </div>
+}
